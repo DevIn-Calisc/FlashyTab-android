493c54153ce82ffa357f2ffb777d6b2b9b247c9f
Hung Cao <cthung.it2013@gmail.com>
2020-04-10 20:44:40 +0700

    Make an example for flash tab

--- FlashyTab/app/src/main/java/com/calisc/flashytab/MainActivity.java
+++ FlashyTab/app/src/main/java/com/calisc/flashytab/MainActivity.java
@@ -2,17 +2,29 @@ package com.calisc.flashytab;
 
 import android.os.Bundle;
 
+import com.calisc.flashytab.FlashyTab.FlashyTab;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Fragment> mFragmentList = new ArrayList<>();
+    private FlashyTab flashyTab = null;
+    private String[] titles = new String[]{"Events", "Highlights", "Search", "Settings"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,52 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        //
+        mFragmentList.add(new TabFragment(titles[0]));
+        mFragmentList.add(new TabFragment(titles[1]));
+        mFragmentList.add(new TabFragment(titles[2]));
+        mFragmentList.add(new TabFragment(titles[3]));
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        FragmentStatePagerAdapter adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragmentList.size();
+            }
+        };
+        viewPager.setAdapter(adapter);
+
+        // Generate tab
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        flashyTab = new FlashyTab(tabLayout);
+        flashyTab.addTabItem(titles[0], R.drawable.ic_events, null, null);
+        flashyTab.addTabItem(titles[1], R.drawable.ic_highlights, null, null);
+        flashyTab.addTabItem(titles[2], R.drawable.ic_search, null, null);
+        flashyTab.addTabItem(titles[3], R.drawable.ic_settings, null, null);
+        flashyTab.highLightTab(0);
+
+        viewPager.addOnPageChangeListener(flashyTab);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        flashyTab.onStart((TabLayout) findViewById(R.id.tabLayout));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        flashyTab.onStop();
     }
 
     @Override
---
+++ FlashyTab/app/src/main/java/com/calisc/flashytab/TabFragment.java
@@ -0,0 +1,33 @@
+package com.calisc.flashytab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TabFragment extends Fragment {
+    private String title = "";
+    public TabFragment(String title) {
+        this.title = title;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setRetainInstance(true);
+
+        return inflater.inflate(R.layout.fragment_content, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // update content
+
+    }
+}
